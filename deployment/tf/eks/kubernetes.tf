771ea5f40fb351c6a8bd901bcef64b9f3be08a55
berrys <berrys.sh@gmail.com>
2024-10-05 16:25:43 +0300

    tf -> join nodes

---
+++ deployment/tf/eks/kubernetes.tf
@@ -0,0 +1,22 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = <<EOF
+- rolearn: ${aws_iam_role.node_instance_role.arn}
+  username: system:node:{{EC2PrivateDNSName}}
+  groups:
+    - system:bootstrappers
+    - system:nodes
+EOF
+  }
+
+  depends_on = [aws_eks_cluster.demo_eks]
+}
