d65a0f7036c6fe8d39924969ef408b1cc0274aef
berrys <berrys.sh@gmail.com>
2024-10-05 16:53:45 +0300

    tf -> join nodes 2

--- deployment/tf/eks/kubernetes.tf
+++ deployment/tf/eks/kubernetes.tf
@@ -2,6 +2,15 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
+resource "null_resource" "update_kubeconfig" {
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --region us-east-1 --name ${aws_eks_cluster.demo_eks.name}"
+  }
+
+  depends_on = [aws_eks_cluster.demo_eks]
+}
+
+
 resource "kubernetes_config_map" "aws_auth" {
   metadata {
     name      = "aws-auth"
