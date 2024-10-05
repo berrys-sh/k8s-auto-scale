7b4d4c217a8fdd76b0218bc08da270c735250f29
berrys <berrys.sh@gmail.com>
2024-10-05 08:43:32 +0300

    tf -> fix validate

--- deployment/tf/eks/outputs.tf
+++ deployment/tf/eks/outputs.tf
@@ -1,13 +1,9 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = aws_vpc.demo_vpc.id
+  value       = data.aws_vpc.demo_vpc.id
 }
 
 output "public_subnet_ids" {
-  description = "The IDs of the public subnets"
-  value = [
-    aws_subnet.public_subnet_1.id,
-    aws_subnet.public_subnet_2.id,
-    aws_subnet.public_subnet_3.id
-  ]
+  description = "List of IDs of public subnets"
+  value       = aws_subnet.public[*].id
 }
