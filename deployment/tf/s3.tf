fa48d8406963a21676aefdfe48cdffd97a4b34f6
berrys <berrys.sh@gmail.com>
2024-10-12 15:03:56 +0300

    tf -> add s3 2

--- deployment/tf/s3.tf
+++ deployment/tf/s3.tf
@@ -6,3 +6,18 @@ resource "aws_s3_bucket" "wc_server_dev" {
     Environment = "dev"
   }
 }
+resource "aws_s3_bucket_policy" "wc_server_dev_policy" {
+  bucket = aws_s3_bucket.wc_server_dev.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.wc_server_dev.arn}/*"
+      }
+    ]
+  })
+}
