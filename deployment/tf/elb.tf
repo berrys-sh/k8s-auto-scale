921fd387cc5924e4d1f389b83428aa6cdec5007f
berrys <berrys.sh@gmail.com>
2024-10-12 13:03:55 +0300

    tf -> add elb 6

--- deployment/tf/elb.tf
+++ deployment/tf/elb.tf
@@ -61,5 +61,5 @@ resource "aws_lb_listener" "app_listener" {
 
 resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = aws_cloudformation_stack.autoscaling_group.outputs["NodeAutoScalingGroup"]
-  alb_target_group_arn   = aws_lb_target_group.app_tg.arn
+  lb_target_group_arn   = aws_lb_target_group.app_tg.arn
 }
